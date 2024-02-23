c92a0e0f6b8dcd4f42a6def2e66e41ce0765245d
Manuvai <44281637+manuvai@users.noreply.github.com>
2024-02-23 22:57:57 +0100

    feat: V1

--- java-to-plantuml.py
+++ java-to-plantuml.py
@@ -1,5 +1,6 @@
 import argparse
 from enum import Enum
+import glob
 import re
 
 class Visibility(Enum):
@@ -100,6 +101,9 @@ re_attributes = r"^\s*(public|private|protected)?\s+(?!class|return|package)(\w+
 
 re_parameters = r'\b\w+(?:<\w+>)?\b(?=\s+\w+|$)'
 
+re_package = r'^package\s+([a-zA-Z_][a-zA-Z0-9_]*(?:\.[a-zA-Z_][a-zA-Z0-9_]*)*);\s*$'
+re_class = r'^\s*(?:public\s+)?(?:abstract\s+)?(?:final\s+)?(?:strictfp\s+)?class\s+([a-zA-Z_][a-zA-Z0-9_]*)'
+
 def get_attributes(content: str) -> list[Attribute]:
     attributes = []
 
@@ -138,13 +142,106 @@ def get_methods(content: str) -> list[Attribute]:
 
     return methods
 
+def get_class(content: str) -> JavaClass:
+    matches = re.search(re_class, content, re.MULTILINE)
+
+    return None if not matches else JavaClass(matches.group(1))
+
+def get_package(content: str) -> Package:
+    matches = re.search(re_package, content, re.MULTILINE)
+
+    return None if not matches else Package(matches.group(1))
+
 
 class PlantUmlAdapter:
     def __init__(self, content: str) -> None:
         self.content = content
         # TODO Faire en sorte que pour chaque contenu, on puisse extraire la classe => penser au package
 
+    def extract(self) -> tuple[Package, JavaClass]|None:
+        attributes = get_attributes(self.content)
+        methods = get_methods(self.content)
+        java_class = get_class(self.content)
+        java_package = get_package(self.content)
+
+        if (not java_class and not java_package):
+            return None
+        
+        java_class.attributes = attributes
+        java_class.methods = methods
+
+        return java_package, java_class
+
+class Extractor:
+    def __init__(self, path: str) -> None:
+        self.path = path
+
+    def list_files(self) -> list[str]:
+        files = glob.glob(self.path + "/**/*.java", recursive=True)
+
+        files = [_.replace('\\', '/') for _ in files]
+        return files
     
+    def execute(self, output: str) -> None:
+        def __write(content: str, path: str):
+            with open(path, "w") as file:
+                file.write(content)
+
+        def __get_parent(name: str) -> str|None:
+            last_dot_index = name.rfind('.')
+            
+            # Extract the parent package name
+            parent_name = name[:last_dot_index] if last_dot_index != -1 else None
+            
+            return parent_name
+
+        def __get_content(path: str) -> str:
+            with open(path, "r", encoding="utf-8") as f:
+                lines = f.readlines()
+            
+            return "\n".join(lines)
+
+        def __redo_packages(packages: dict[str, Package]) -> list[Package]:
+            new_packages: dict[str, Package] = {}
+
+            for key in sorted(packages):
+                package_parent = __get_parent(key)
+                if (package_parent in new_packages):
+                    new_packages[package_parent].packages.append(packages[key])
+                else:
+                    new_packages[key] = packages[key]
+            
+            return [_ for _ in new_packages.values()]
+
+
+        packages: dict[str, Package] = {}
+
+        files = self.list_files()
+
+        for file in files:
+            content = __get_content(file)
+            adapter = PlantUmlAdapter(content)
+
+            package, java_class = adapter.extract()
+
+            key = "__default__"
+            if (package is not None):
+                key = package.name
+
+            if (key not in packages):
+                packages[key] = package
+
+            packages[key].classes.append(java_class)
+
+        packages_list = __redo_packages(packages)
+
+        packages_content = [_.__str__() for _ in packages_list]
+
+        content = "\n".join(packages_content)
+
+        lib_url = "!includeurl https://raw.githubusercontent.com/manuvai/plantuml-utils/master/class_diagram_utils.puml"
+        content = "@startuml\n\n" + lib_url + "\n" + content
+        __write(content, output)
 
 def test_regex():
     lines = []
@@ -167,7 +264,16 @@ def main():
     args = parser.parse_args()
     print(args.path)
     print(args.output)
-    test_regex()
+
+    extractor = Extractor(args.path)
+    #TODO Fix the fact that it doesnt fucking work
+    # print(extractor.list_files())
+    extractor.execute(args.output)
+
+    # test_regex()
+# files = glob.glob(self.path + "\**\*.java", recursive=True)
+# C:\Users\rm902\eclipse-workspace\DAI\TpRttEtd\
+# C:/Users/rm902/eclipse-workspace/DAI/TpRttEtd/
 
 def test():
     a1 = Attribute("attribut1", "int", Visibility.PRIVATE)
